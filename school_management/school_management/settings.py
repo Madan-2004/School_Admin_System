09c860ae794540d598d4904f11cf12feefde3a81
Madan-2004 <madansivam17@gmail.com>
2023-11-14 10:50:48 +0530

    hello

--- school_management/school_management/settings.py
+++ school_management/school_management/settings.py
@@ -74,16 +74,16 @@ WSGI_APPLICATION = 'school_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'school_management',
-        'USER': 'root',
-        'PASSWORD': 'Pavanshiva@96',
-        'HOST':'localhost',
-        'PORT':'3306',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'school_management',
+#         'USER': 'root',
+#         'PASSWORD': 'Pavanshiva@96',
+#         'HOST':'localhost',
+#         'PORT':'3306',
+#     }
+# }
 
 
 # Password validation
